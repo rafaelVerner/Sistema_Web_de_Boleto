ac303741ffc8048846cdb076144ad72f6c91b493
Verner Rafael <vernerrrferreira@gmail.com>
2025-07-16 22:56:50 -0300

    Conectando com o DB

--- api/dataBase.php
+++ api/dataBase.php
@@ -1,9 +1,9 @@
 <?php
 
-$db_host = 'localhost';
+$db_host = 'mysql.railway.internal';
 $db_name = 'cobranca';
 $db_user = 'root';
-$db_pass = '';
+$db_pass = 'cgkVRdBzJYunyIcdHudfCNBiOTLOmcPe';
 
 try{
     $pdo = new PDO("mysql:host={$db_host};dbname={$db_name}", $db_user, $db_pass);
